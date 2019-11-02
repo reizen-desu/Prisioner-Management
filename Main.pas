0993911abeaa238b057b8004b08aac0ac4f9838f
reizen-desu <nokiaq80@gmail.com>
2019-11-02 18:56:15 +0200

    Added Visitors Data

--- Main.pas
+++ Main.pas
@@ -2,9 +2,16 @@ Program Cryme;
 Uses Crt, SysUtils, DateUtils, DOS;
 
 Type
+	{Variables used by the Prisoner. Necessary to the register form}
 	prisonerData = Record
 		name: String[50];
 	end;
+
+	{Variables used by the Visitor. Necessary to the register form}
+	visitorData = Record
+		name: String[50];
+	end;
+
 
 Begin
 	Writeln('Hello world');
