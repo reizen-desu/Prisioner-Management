a23cef69f7e519b62b249c0d9b198a4035638717
reizen-desu <nokiaq80@gmail.com>
2019-11-03 20:19:37 +0200

    Added Administrator

--- Main.pas
+++ Main.pas
@@ -2,6 +2,11 @@ Program Cryme;
 Uses Crt, SysUtils, DateUtils, DOS;
 
 Type
+	{Variables used by the Administrator. Necessary to the first run}
+	adminData = Record
+		name: String;
+	end;
+
 	{Variables used by the Prisoner. Necessary to the register form}
 	prisonerData = Record
 		name: String[50];
